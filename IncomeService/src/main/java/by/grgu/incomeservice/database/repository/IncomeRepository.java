ffe6ff37c2a22d388383bd4ab0303d01b43bb399
kaleria <dybovik.val@gmail.com>
2025-06-04 17:57:16 +0300

    Work

--- IncomeService/src/main/java/by/grgu/incomeservice/database/repository/IncomeRepository.java
+++ IncomeService/src/main/java/by/grgu/incomeservice/database/repository/IncomeRepository.java
@@ -11,17 +11,19 @@ import java.util.List;
 @Repository
 public interface IncomeRepository extends JpaRepository<Income, Long> {
 
-    List<Income> findByUsername(String username); // ✅ Запрос доходов по `username`
+    List<Income> findByUsername(String username);
 
     @Query("SELECT i FROM Income i WHERE i.username = :username AND i.date BETWEEN :startDate AND :endDate")
-    List<Income> getIncomesForMonth(@Param("username") String username, @Param("startDate") LocalDate startDate, @Param("endDate") LocalDate endDate);
+    List<Income> getIncomesForMonth(@Param("username") String username, @Param("startDate") LocalDate startDate,
+                                    @Param("endDate") LocalDate endDate);
 
     List<Income> findByUsernameAndDateBetween(String username, LocalDate startDate, LocalDate endDate);
 
     @Query("SELECT DISTINCT i.source FROM Income i WHERE i.username = :username")
     List<String> getDistinctIncomeSources(@Param("username") String username);
 
-    @Query("SELECT i FROM Income i WHERE i.username = :username AND i.source = :source AND i.date BETWEEN :startDate AND :endDate")
+    @Query("SELECT i FROM Income i WHERE i.username = :username " +
+            "AND i.source = :source AND i.date BETWEEN :startDate AND :endDate")
     List<Income> getIncomeBySourceForMonth(@Param("username") String username,
                                            @Param("source") String source,
                                            @Param("startDate") LocalDate startDate,
--- identity-service/src/main/java/by/grgu/identityservice/controller/AuthController.java
+++ identity-service/src/main/java/by/grgu/identityservice/controller/AuthController.java
@@ -21,7 +21,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.net.URI;
 import java.util.List;
